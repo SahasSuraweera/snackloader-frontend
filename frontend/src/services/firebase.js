a991bda04e76beb067840c43a49cb880dfe8045c
Sahas Suraweera <sahassuraweera11@gmail.com>
2025-11-18 14:21:59 +0530

    Added login page and firebase configuration

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,17 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER",
-  appId: "YOUR_APP",
+  apiKey: "AIzaSyCxxFplPvmKgSn4YIym7jhBiVxhSu_nzeQ",
+  authDomain: "snackloader.firebaseapp.com",
+  projectId: "snackloader",
+  storageBucket: "snackloader.firebasestorage.app",
+  messagingSenderId: "207394701592",
+  appId: "1:207394701592:web:f04908c7acddc0170a1e6d",
+  measurementId: "G-Z7FDLRCBWG"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
